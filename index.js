abaa571e875b95bcb91e275f666362df319eace9
Aric Craciun <aric.craciun@thereformation.us>
2026-04-20 17:17:22 -0400

    added a couple of exports for legacy automated tests

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ module.exports.SfccClient      = require('./sfcc/sfcc-client');
 // Deposco
 module.exports.DeposcoApiClient      = require('./deposco/deposco-api-client');
 module.exports.DeposcoApiShipment    = require('./deposco/deposco-api-shipment');
+
+// Legacy imports — for testing use only
+module.exports.InventoryHelper             = require('./sfcc/ocapi/data-api/util/inventory-helper');
+module.exports.OcapiHelper                 = require('./sfcc/ocapi/ocapi-helper');
+module.exports.RefInventoryProductRecord   = require('./sfcc/ref-inventory-product-record');
